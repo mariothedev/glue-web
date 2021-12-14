0b39717e156fa6c023da53d7e002ed69a975f05c
Mario <mariothedev@gmail.com>
2021-12-14 17:27:29 +0100

    Remove unused pages and components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,38 @@
 import type { NextPage } from 'next'
+import { useEffect } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-import Main from '../features/main/Main'
+import { useRouter } from 'next/router'
+import { useSession, signIn } from 'next-auth/react'
+
 const IndexPage: NextPage = () => {
+
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (session && status === 'authenticated') {
+      router.push({ pathname: '/account', query: { email: session.user.email } })
+    }
+  }, [session, status])
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Redux Toolkit</title>
+        <title>Glue: vocabulary app</title>
         <link rel="icon" href="/favicon.ico" />
-        {/* Acquire new vocabulary efficiently. */}
+        <meta name="description" content="Learn new vocabulary efficiently on the go."/>
       </Head>
       <main className={styles.header}>
-        <Main />
+        <div>
+          {!session && status !== 'loading' &&
+            <button onClick={() => signIn()}> Sign in</button>
+          }
+          {status === 'loading' &&
+            <p> Loading... </p>
+          }
+        </div>
       </main>
     </div>
   )
